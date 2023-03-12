24eedc06d69e06aefc6fec1b3d76f67d04a5377c
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-03-12 16:14:14 +0600

    form valllidation

--- user/user.js
+++ user/user.js
@@ -1,8 +1,19 @@
-// geeting inpt value
-function gettingInputValue (id){
+// geeting input value
+function gettingInputValue(id) {
     const inputField = document.getElementById(id);
     const inputValue = inputField.value;
     console.log(inputValue);
+    return inputValue;
 }
 
-gettingInputValue('email-signup');
+
+const signUpBtn = document.getElementById('signup-btn-id').addEventListener('click', function () {
+    const signupName = gettingInputValue('signup-name-id');
+    console.log(typeof(signupName));
+    const signupPwd = gettingInputValue('pwd-id');
+    formVallidation(signupName, signupPwd);
+})
+
+
+
+

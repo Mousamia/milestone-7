9fa718c774b340b3f6aa6f87cae35c9b8815f992
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-03-16 17:49:53 +0600

    local storage

--- local-storage.js
+++ local-storage.js
@@ -5,10 +5,43 @@ const elements = (id) => document.getElementById(id);
 
 
 const addProduct = () => {
-    console.log(elements('product-name').value);
+    const productName = elements('product-name').value;
+    const productquantity = elements('product-quanity').value
 
-    const productkey = (elements('product-name').value);
-    const productquantity = elements('product-quantity').value;
-    console.log(productkey, productquantity);
+    console.log(productName, productquantity);
+    const ul = document.getElementById('container');
+    const li = document.createElement('li');
+    li.innerText = `${productName} ${productquantity}
+    `;
+
+    ul.appendChild(li);
+    getLoacalStorage();
+    saveProductToLocalStorage(productName, productquantity);
+}
+
+// checking if the item exists in localstorage
+
+const getLoacalStorage = () => {
+    let cart = {};
+    let storedCart = localStorage.getItem('cart');
+    // let samia = localStorage.getItem('samia');
+    console.log(cart);
+    if(storedCart){
+        cart = JSON.parse(storedCart);
+        // cart = JSON.parse(samia);
+    }
+
+    return cart;
+}
+
+
+const saveProductToLocalStorage = (productName, productquantity) => {
+    const cart = getLoacalStorage();
+    console.log(cart);
+    cart[productName] = productquantity;
+
+    const stringyfied = JSON.stringify(cart);
+    localStorage.setItem("cart", stringyfied);
+    console.log(cart);
 
 }
